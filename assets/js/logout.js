e4785414a4d64314227b177a17c82f0f56a633cd
pandimaya <alexdimagician99@gmail.com>
2023-11-20 20:58:53 +0800

    try cache

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -1,14 +1,17 @@
-document.getElementById('logout').addEventListener('click', async function(e) {
+document.getElementById('logout').addEventListener('click', async function (e) {
   const res = await fetch('/logout', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     }
-  })
+  });
 
-  const json = await res.json()
+  const json = await res.json();
 
   if (json.status === 200) {
-    window.location.href = '/'
+    // Clear local session data or perform any cleanup
+    document.cookie = 'userData=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    // Redirect to the home page
+    window.location.href = '/';
   }
-})
+});
