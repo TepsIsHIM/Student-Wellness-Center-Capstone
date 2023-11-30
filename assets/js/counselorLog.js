33e6e14729c80855f486bf055a2506941baee2a2
Stephen Manalo <stephamanalo@gmail.com>
2023-11-30 20:45:48 +0800

    Counselor Log Table

--- assets/js/counselorLog.js
+++ assets/js/counselorLog.js
@@ -14,3 +14,17 @@ function toggleRow(rowId) {
     const hiddenRow = document.getElementById(rowId);
     hiddenRow.style.display = (hiddenRow.style.display === "table-row") ? "none" : "table-row";
 }
+function filterTable(column, value) {
+  var rows = document.getElementById("TableID").getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+  for (var i = 0; i < rows.length; i++) {
+      var currentRow = rows[i].getElementsByTagName("td")[column];
+      if (currentRow) {
+          var textValue = currentRow.textContent || currentRow.innerText;
+          if (textValue.indexOf(value) > -1) {
+              rows[i].style.display = "";
+          } else {
+              rows[i].style.display = "none";
+          }
+      }
+  }
+}
