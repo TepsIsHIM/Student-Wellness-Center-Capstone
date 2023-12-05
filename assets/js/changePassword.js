3342ae0e6359921958a104fff2db37d951e2d314
pandimaya <alexdimagician99@gmail.com>
2023-12-06 06:40:34 +0800

    CHANGE

--- assets/js/changePassword.js
+++ assets/js/changePassword.js
@@ -1,43 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const changeForm = document.getElementById('changeForm');
     const newPasswordInput = document.getElementById('newPassword');
     const confirmPasswordInput = document.getElementById('confirmPassword');
+    const confirmButton = document.getElementById('confirmButton');
 
-    if (changeForm) {
-        const confirmButton = document.getElementById('confirmButton');
+    confirmButton.addEventListener('click', async function () {
+        const newPassword = newPasswordInput.value.trim();
+        const confirmPassword = confirmPasswordInput.value.trim();
 
-        confirmButton.addEventListener('click', async function () {
-            const newPassword = newPasswordInput.value.trim();
-            const confirmPassword = confirmPasswordInput.value.trim();
+        if (newPassword !== confirmPassword) {
+            // Passwords do not match
+            alert('Passwords do not match');
+            return;
+        }
 
-            if (newPassword !== confirmPassword) {
-                // Passwords do not match
-                alert('Passwords do not match');
-                return;
+        try {
+            const response = await fetch('/changePassword', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ newPassword }),
+            });
+
+            if (response.ok) {
+                // Password changed successfully
+                const message = await response.text();
+                alert(message); // You can customize this alert or redirect the user to a success page
+            } else {
+                // Handle other response statuses
+                const errorMessage = await response.text();
+                alert(`Error: ${errorMessage}`);
             }
-
-            try {
-                const response = await fetch('/changePassword', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ newPassword }),
-                });
-
-                if (response.ok) {
-                    // Password changed successfully
-                    const message = await response.text();
-                    alert(message); // You can customize this alert or redirect the user to a success page
-                } else {
-                    // Handle other response statuses
-                    const errorMessage = await response.text();
-                    alert(`Error: ${errorMessage}`);
-                }
-            } catch (error) {
-                console.error('Unexpected error:', error);
-                alert('Failed to change password. Please try again.'); // Provide user-friendly error message
-            }
-        });
-    }
+        } catch (error) {
+            console.error('Unexpected error:', error);
+            alert('Failed to change password. Please try again.'); // Provide user-friendly error message
+        }
+    });
 });
